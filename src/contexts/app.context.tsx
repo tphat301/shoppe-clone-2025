af49af4ef317dd3860cdd81a7e5ce98bab1fe34d
Do Lam Thanh Phat <dolamthanhphat.tmt@gmail.com>
2025-02-24 21:55:10 +0700

    Shoppe clone code reset extendedPurchase when access token expired

--- src/contexts/app.context.tsx
+++ src/contexts/app.context.tsx
@@ -33,6 +33,7 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   const reset = () => {
     setIsAuthenticated(false)
     setProfile(null)
+    setExtendedPurchase([])
   }
   return (
     <AppContext.Provider
