86f9fba997204014aca98ca43aae5d09a374b593
Do Lam Thanh Phat <dolamthanhphat@gmail.com>
2025-03-01 10:42:06 +0700

    Shoppe clone test funtion error utils

---
+++ src/utils/__test__/utils.test.ts
@@ -0,0 +1,35 @@
+import { describe, it, expect } from 'vitest'
+import { isAxiosError, isAxiosUnprocessableEntityError } from '../utils'
+import { AxiosError } from 'axios'
+import { httpStatusCode } from '../../constants/httpStatusCode'
+
+describe('isAxiosError', () => {
+  it('isAxiosError trả về boolean', () => {
+    expect(isAxiosError(new Error())).toBe(false)
+    expect(isAxiosError(new AxiosError())).toBe(true)
+  })
+})
+
+describe('isAxiosUnprocessableEntityError', () => {
+  it('isAxiosUnprocessableEntityError trả về boolean', () => {
+    expect(isAxiosUnprocessableEntityError(new Error())).toBe(false)
+    expect(
+      isAxiosUnprocessableEntityError(
+        new AxiosError(undefined, undefined, undefined, undefined, {
+          status: httpStatusCode.InternalServerError,
+          data: null
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } as any)
+      )
+    ).toBe(false)
+    expect(
+      isAxiosUnprocessableEntityError(
+        new AxiosError(undefined, undefined, undefined, undefined, {
+          status: httpStatusCode.UnprocessableEntity,
+          data: null
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } as any)
+      )
+    ).toBe(true)
+  })
+})
